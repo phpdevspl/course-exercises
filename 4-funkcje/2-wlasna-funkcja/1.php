2c97b06b5f9cce5165e1c2f4d8cbff65010fe66c
Arkadiusz Waluk <arkadiusz@waluk.pl>
2017-03-15 12:54:19 +0100

    Add part 4 lesson 2

---
+++ 4-funkcje/2-wlasna-funkcja/1.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Task: 1. Przygotuj własną funkcję o nazwie showHello wyświetlającą napis "Hello world".
+Lesson: https://www.phpdevs.pl/funkcje/2-wlasna-funkcja
+*/
+
+function showHello()
+{
+    echo 'Hello world';
+}
+
+showHello();
